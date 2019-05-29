19626a68f07edcd065dcb2881f09d1424adaeb5e
theduykh <theduykh@gmail.com>
2019-05-29 15:49:49 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,22 @@
-import ElementUtils from '../utils/ElementUtils'
+import ElementUtils from '../utils/ElementUtils';
 
 class Login {
-  login(username, password) {
-    ElementUtils.element('[name="username"]').setValue(username)
-    ElementUtils.element('[name="password"]').setValue(password)
-    ElementUtils.element('.login-button').click()
-    browser.pause(2133)
+
+  open() {
+    browser.url('login');
+    return this;
   }
+
+  login(username, password) {
+    ElementUtils.element('[name="username"]').setValue(username);
+    ElementUtils.element('[name="password"]').setValue(password);
+    ElementUtils.element('button[type="submit"]').click();
+  }
+
+  getMessage() {
+    return ElementUtils.element('#flash').getText();
+  }
+
 }
-module.exports = new Login()
+
+export default new Login();
